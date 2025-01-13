bc29df73c69b097afea26e9b85e987ffb4fef650
Jack Balster <107078414+jack-balster@users.noreply.github.com>
2025-01-13 15:16:02 -0800

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Jack. I'm a graduate student at Washington State University, pursuing a Master's in Software Engineering.",
-    description2: "With a passion for software engineering, I'm interested in all types of development having used technologies like HTML, CSS, JavaScript, Python, C++, and C#. I'm familiar with frameworks such as React, Django, Flask, and Node.js, and databases such as MySQL and MongoDB. I maintained a 3.8 GPA and was on the President's Honor Roll all throughout my undergraduate studies showcasing my work ethic, teamwork, and consistency.",
+    description1: "My name's Jack. I'm a graduate student at Washington State University, pursuing a Master's in Software Engineering. I'm also currently working as a full stack software engineer intern for TORQ Sports.",
+    description2: "With a passion for software engineering, I'm interested in all types of development having used technologies like HTML, CSS, JavaScript, Python, Dart, C++, C#, Firebase, and much more. I'm also familiar with frameworks such as React, Django, Flask, Flutter, Node.js, Tailwind CSS, Bootstrap, and databases such as MySQL and MongoDB. I maintained a 3.8 GPA and was on the President's Honor Roll all throughout my undergraduate studies showcasing my work ethic, teamwork, and consistency.",
     image: 1
 }
